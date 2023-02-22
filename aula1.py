2f42c362dc376424b88ccbc0ffed0549e080a044
Joao Sales <joaovictorlsales@gmail.com>
2023-02-22 17:18:04 -0300

    DocString e comentários

---
+++ aula1.py
@@ -0,0 +1,18 @@
+"""
+Isso é uma DocString
+O interpretador do Python ler isso.
+Também é comumente utilizado para fazer comentários
+de múltiplas linhas.
+"""
+
+'''
+DocString com aspas simples também funciona.
+'''
+
+# Permite escrever um comentário in-line
+print(123)  # Na frente
+# Abaixo
+print(456)
+
+# Interpretador do Python não lê essa linha
+# Isso é um comentário oficial do Python, quando utilizado a cerquilha
