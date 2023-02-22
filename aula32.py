dbe59de51c4224060bd712c1a325894fa12ab5b5
Joao Sales <joaovictorlsales@gmail.com>
2023-02-22 18:50:09 -0300

    Exercícios - Testando o conhecimento até aqui

--- aula32.py
+++ aula32.py
@@ -71,4 +71,12 @@ menos escreva "Seu nome é curto"; se tiver entre 5 e 6 letras, escreva
 "Seu nome é normal"; maior que 6 escreva "Seu nome é muito grande".
 """
 
-nome = input('')
+nome = input('Digite o seu primeiro nome: ')
+tamanho_nome = len(nome)
+
+if tamanho_nome <= 4:
+    print('Seu nome é curto')
+elif 5 <= tamanho_nome <= 6:
+    print('Seu nome é normal')
+else:
+    print('Seu nome é muito grande')
