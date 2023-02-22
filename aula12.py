c3276d98252c040e961ca427b50ede24dcfedc33
Joao Sales <joaovictorlsales@gmail.com>
2023-02-22 17:31:46 -0300

    Exercício Cálculo do IMC

---
+++ aula12.py
@@ -0,0 +1,11 @@
+nome = 'Joao Sales'
+altura = 1.75
+peso = 80
+imc = peso / altura ** 2
+
+print(nome, 'tem', altura, 'de altura,')
+print('pesa', peso, 'quilos e seu IMC é')
+print(imc)
+# Joao Sales tem 1.75 de altura,
+# pesa 80 quilos e seu IMC é
+# 26.12244897959184
