25517ac6161b8e76d28ba29de9a76c58d4e2cd5a
Joao Sales <joaovictorlsales@gmail.com>
2023-02-22 17:23:25 -0300

    Strings e aspas

---
+++ aula3.py
@@ -0,0 +1,21 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+
+# Aspas simples
+print('João Sales')
+print(1, 'João "Sales"')
+
+# Aspas duplas
+print("João Sales")
+print(2, "João 'Sales'")
+
+# Escape
+print("João \"Sales\"")
+
+# r
+print(r"João \"Sales\"")
