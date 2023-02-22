4e631e954c8241e72f681fac327d69a0a0c208e4
Joao Sales <joaovictorlsales@gmail.com>
2023-02-22 17:27:04 -0300

    Exercício com variáveis

---
+++ aula8.py
@@ -0,0 +1,14 @@
+nome = 'João'
+sobrenome = 'Sales'
+ano_nascimento = 1975
+ano_atual = 2023
+idade = ano_atual - ano_nascimento
+maior_de_idade = idade >= 18
+altura_metros = 1.80
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de Nascimento:', ano_nascimento)
+print('É maior de idade ?', maior_de_idade)
+print('Altura em metros:', altura_metros)
