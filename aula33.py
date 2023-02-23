0691bdbe79a46f3aa8d748ddaab4a0dd4c744c2a
Joao Sales <joaovictorlsales@gmail.com>
2023-02-22 22:34:11 -0300

    Built-in types, doc. python, tipos imutáveis

---
+++ aula33.py
@@ -0,0 +1,11 @@
+"""
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutáveis que vimos: str, int, float, bool
+"""
+
+string = 'Joao Sales'
+outra_string = f'{string[:3]}ABC{string[4:]}'
+print(string)
+print(outra_string)
+# print(string.capitalize())
+# print(string.zfill(100))
