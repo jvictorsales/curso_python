6126d340486079cab95d58848e95cbf9595109f6
Joao Sales <joaovictorlsales@gmail.com>
2023-03-09 16:28:56 -0300

    Introdução ao tipo list (listas)

--- aula48.py
+++ aula48.py
@@ -8,12 +8,12 @@ Métodos úteis: append, insert, pop, del, clear, extend, +
 #........+01234
 #........-54321
 string = 'ABCDE'  # 5 caracteres (len)
-#.........0....1......2...........3....4
-#........-5...-4.....-3..........-2...-1
+
+#.........0....1......2..........3.....4
+#........-1...-2.....-3.........-4....-5
 lista = [123, True, 'Joao Sales', 1.2, []]
 # print(bool(lista))  # falsy
 # print(lista, type(lista))
-# print(lista[2].upper(), type(lista[2]))
 lista[-3] = 'Maria'
 print(lista)
 print(lista[2], type(lista[2]))
