3d40d517534d3352ae064b3ca2c6b4606d21e3ae
Joao Sales <joaovictorlsales@gmail.com>
2023-03-09 16:48:49 -0300

    Alterando uma lista com índices, del, append e pop

---
+++ aula49.py
@@ -0,0 +1,23 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append, insert, pop, del, clear, extend, +
+Create Read Update Delete
+Criar, ler, alterar, apagar = lista[i] (CRUD)
+"""
+
+#........0...1...2...3...4...5...6
+lista = [10, 20, 30, 40, 50, 60, 70]
+# lista[2] = 300
+# del lista[2]
+# print(lista)
+# print(lista[2])
+lista.append(80)
+lista.pop()
+lista.append(90)
+lista.append(100)
+ultimo_valor = lista.pop(3)
+print(lista, 'Removido', ultimo_valor)
