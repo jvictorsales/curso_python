7ad886329ee4f24d8ac4baf5014b400ecd882cca
Joao Sales <joaovictorlsales@gmail.com>
2023-03-10 23:02:16 -0300

    Exercício - Lista de Compras

--- aula58.py
+++ aula58.py
@@ -12,47 +12,51 @@ lista_compras = []
 
 while True:
     print('Selecione uma opção')
-    opcao = input('[i]nserir [a]pagar [l]istar [s]air: ').lower()
-    os.system('cls')
 
-    if opcao.startswith('i'):
-        add_item = input('Digite o produto: ')
-        print(f'Produto "{add_item}" adicionado na lista de compras.')
-        lista_compras.append(add_item)
-        print()
+    try:
+        opcao = input('[i]nserir [a]pagar [l]istar [s]air: ').lower()
+        os.system('cls')
 
-    elif opcao.startswith('a'):
-        if lista_compras == []:
-            print('Lista vazia, não tem o que apagar.')
-            continue
-        
-        remove_item = input('Digite o índice do produto para apagar: ')
-        
-        if not remove_item.isdigit():
-            print('Esse índice não existe, tente novamente.')
-            continue
-        
-        remove_item = int(remove_item)
-        
-        print(f'Produto "{lista_compras[remove_item]}" removido.')
-        del(lista_compras[remove_item])
-        print()
+        if opcao.startswith('i'):
+            add_item = input('Digite o produto: ')
+            print(f'Produto "{add_item}" adicionado na lista de compras.')
+            lista_compras.append(add_item)
+            print()
 
-    elif opcao.startswith('l'):
-        if lista_compras == []:
-            print('Lista vazia, não tem o que exibir.')
-            continue
-        for indice, produto in enumerate(lista_compras):
-            print(indice, produto)
-        print()
+        elif opcao.startswith('a'):
+            if lista_compras == []:
+                print('Lista vazia, não tem o que apagar.')
+                continue
 
-    elif opcao.startswith('s'):
-        print('Encerrando... ')
-        print()
-        break
-        
-    else:
-        print('Opção inválida!')
+            remove_item = input('Digite o índice do produto para apagar: ')
+            remove_item = int(remove_item)
+            
+            if remove_item < 0:
+                print('Índice inválido, tente novamente.')
+                continue
+
+            print(f'Produto "{lista_compras[remove_item]}" removido.')
+            del(lista_compras[remove_item])
+            print()
+
+        elif opcao.startswith('l'):
+            if lista_compras == []:
+                print('Lista vazia, não tem o que exibir.')
+                continue
+            for indice, produto in enumerate(lista_compras):
+                print(indice, produto)
+            print()
+
+        elif opcao.startswith('s'):
+            print('Encerrando... ')
+            print()
+            break
+            
+        else:
+            print('Opção escolhida inválida! Tente novamente.')
+
+    except:
+        print('Índice inválido, tente novamente.')
 
 # lista_compras = []
 
