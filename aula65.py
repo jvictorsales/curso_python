6d714064f3a8112d54065e8896296cd42fe5c8ce
Joao Sales <joaovictorlsales@gmail.com>
2023-03-15 10:24:39 -0300

    Cálculo do primeiro dígito do CPF

--- aula65.py
+++ aula65.py
@@ -29,8 +29,8 @@ nove_digitos = cpf[:9]
 contador_regressivo_1 = 10
 
 resultado_digito_1 = 0
-for digito_1 in nove_digitos:
-    resultado_digito_1 += int(digito_1) * contador_regressivo_1
+for digito in nove_digitos:
+    resultado_digito_1 += int(digito) * contador_regressivo_1
     contador_regressivo_1 -= 1
 
 digito_1 = (resultado_digito_1 * 10) % 11
