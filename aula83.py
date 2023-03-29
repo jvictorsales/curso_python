5b6c97edceed3a58e1a14438400f0f10d764cadf
Joao Sales <joaovictorlsales@gmail.com>
2023-03-29 10:37:29 -0300

    Exercício - Sistema de perguntas e respostas

--- aula83.py
+++ aula83.py
@@ -19,21 +19,65 @@ perguntas = [
     },
 ]
 
-for questao in perguntas:
-    print('Pergunta:', questao.get('Pergunta'))
+# for questao in perguntas:
+#     print('Pergunta:', questao.get('Pergunta'))
+#     print()
+#
+#     print('Opções:')
+#     opcoes = questao.get('Opcoes')
+#     for indice, opcao in enumerate(opcoes):
+#         print(f'{indice}) {opcao}')
+#    
+#     resposta_indice = input('Escolha uma opção: ')
+#     
+#     try:
+#         resposta_indice = int(resposta_indice)
+#         resposta_correta = questao.get('Resposta')
+#
+#         if opcoes[resposta_indice] == resposta_correta:
+#             print('Acertou!')
+#         else:
+#             print('Errou!')
+#
+#     except ValueError:
+#         print('Opção inválida!')
+#     except IndexError:
+#         print('Esse opção não existe!')
+#
+#     print()
+
+qtd_acertos = 0
+for pergunta in perguntas:
+    print('Pergunta:', pergunta['Pergunta'])
     print()
-    opcoes = questao.get('Opcoes')
-    print('Opções:')
-    for indice, opcao in enumerate(opcoes):
-        print(f'{indice}) {opcao}')
-    resposta_indice = input('Escolha uma opção: ')
-    try:
-        resposta_indice = int(resposta_indice)
-        resposta_correta = questao.get('Resposta')
-        if opcoes[resposta_indice] == resposta_correta:
-            print('Acertou!')
-        else:
-            print('Errou!')
-    except ValueError:
-        print('Opção inválida!')
+    
+    opcoes = pergunta['Opcoes']
+    for i, opcao in enumerate(opcoes):
+        print(f'{i})', opcao)
+    
     print()
+
+    escolha = input('Escolha uma opção: ')
+    
+    acertou = False
+    escolha_int = None
+    qtd_opcoes = len(opcoes)
+
+    if escolha.isdigit():
+        escolha_int = int(escolha)
+    
+    if escolha_int is not None:
+        if escolha_int >= 0 and escolha_int < qtd_opcoes:
+            if opcoes[escolha_int] == pergunta['Resposta']:
+                acertou = True
+
+    print()
+    if acertou:
+        qtd_acertos += 1
+        print('Acertou 👍')
+    else:
+        print('Errou ❌')    
+    
+    print()
+
+print('Você acertou', qtd_acertos, 'de', len(perguntas), 'perguntas.')
